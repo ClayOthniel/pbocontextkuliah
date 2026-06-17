d7f029e588efc34a4c5101c46c1d8479fe33dc46
ClayOthniel <onihpaul@gmail.com>
2026-06-17 08:53:10 +0700

    Add files via upload

--- AppConfig.java
+++ AppConfig.java
@@ -1,27 +1,11 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+package pbocontextkuliah;
 
 public class AppConfig {
-
-  // 1. Konfigurasi Database PostgreSQL
-  // Ganti "nama_database_kamu" dengan nama database yang kamu buat di Navicat
-  private static final String DB_URL = "jdbc:postgresql://localhost:5432/PrakPBOM";
-
-  // 2. Kredensial Login
-  // Default username PostgreSQL biasanya adalah "postgres"
-  private static final String DB_USER = "postgres";
-
-  // Ganti dengan password yang kamu atur saat menginstall PostgreSQL
-  private static final String DB_PASSWORD = "123";
-
-  /**
-   * Method ini akan dipanggil oleh file DAO (seperti JurusanDAO)
-   * setiap kali mereka butuh jalur komunikasi ke database.
-   */
-  public static Connection getConnection() throws SQLException {
-    // DriverManager akan secara otomatis mencari driver PostgreSQL dan membuka
-    // koneksi
-    return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-  }
+    public static final String HOST = "localhost";
+    public static final String PORT = "5432";
+    public static final String DATABASE = "pbokuliah";
+    
+    // UBAH DUA BARIS DI BAWAH INI SESUAI LAPTOPMU:
+    public static final String USER = "postgres"; 
+    public static final String PASSWORD = "root"; 
 }
